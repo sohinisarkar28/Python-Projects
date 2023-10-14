dba1a9f3b3ddd5746d18897e56e69d3b7dfca692
sohinisarkar28 <96813780+sohinisarkar28@users.noreply.github.com>
2023-10-14 18:23:12 -0400

    Add files via upload

---
+++ Madlib2.py
@@ -0,0 +1,36 @@
+def madlibs():
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+    verb1 = input("Verb: ")
+    verb2 = input("Verb: ")
+    verb3 = input("Verb: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
+    noun6 = input("Noun: ")
+    noun7 = input("Noun: ")
+    noun8 = input("Noun: ")
+    noun9 = input("Noun: ")
+    body_part = input("Body_part: ")
+
+    madlib = f"""Coffeehouses are in! Gone are the local corner {noun1} and
+    the neighborhood ice-cream {noun2}. It doesn’t matter if you live in a/an
+    {adj1} city or a/an {adj2} town; there is bound to be a coffee {noun3}
+    in your adj neighborhood. Coffeehouses have become the place where {adj3}
+    friends gather, sit, and chew the {verb3}, remembering the good old {noun4}
+    as they sip their steaming cups of {noun8}. Coffeehouses cater to busy
+    business, who use them to {verb2} million- deals. Coffeehouses are also
+    favorite spots for single men and {noun5}, who love to linger over their
+    mugs of {noun9} as they watch the attractive go by, hoping to catch his
+    or her , and maybe even {verb1} a date. Most evenings, coffeehouses
+    are filled by young lovers drinking out of each other's {noun6} as
+    they whisper sweet {noun7} in each other’s {body_part}."""
+
+    print(madlib)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import Madlib1
+import Madlib2
+import random
+
+
+if __name__ == "__main__":
+    m = random.choice([Madlib1, Madlib2])
+    m.madlibs()
